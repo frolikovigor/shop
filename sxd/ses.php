f0c519928b9c8b846571f3f814f5d262a2386afe
frolikovigor <frolikovigor@gmail.com>
2017-10-07 23:02:18 +0300

    Start project state

---
+++ sxd/ses.php
@@ -0,0 +1,31 @@
+<?php
+$SES = array (
+  '6658407f22b8232514c0c6089eeb5abb' => 
+  array (
+    'cfg' => 
+    array (
+      'charsets' => 'cp1251 utf8 latin1',
+      'lang' => 'ru',
+      'time_web' => '600',
+      'time_cron' => '600',
+      'backup_path' => 'backup/',
+      'backup_url' => 'backup/',
+      'only_create' => 'MRG_MyISAM MERGE HEAP MEMORY',
+      'globstat' => 0,
+      'my_host' => 'localhost',
+      'my_port' => 3306,
+      'my_user' => 'shop',
+      'my_pass' => 'shop',
+      'my_comp' => 0,
+      'my_db' => '',
+      'auth' => 'mysql cfg',
+      'user' => '',
+      'pass' => '',
+      'confirm' => '6',
+      'exitURL' => './',
+    ),
+    'time' => 1507406146,
+    'lng' => 'ru',
+  ),
+);
+?>
